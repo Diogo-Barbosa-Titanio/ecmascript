c861247b7ed9dca23bc1ecfb7668a8ae45847e88
Diogo Barbosa Silva Sousa <diogo.barbosa@gm5.com.br>
2019-12-12 23:22:11 -0300

    Alura - jQuery parte 1 - Aula 05

--- resources/views/alura-typer/principal.blade.php
+++ resources/views/alura-typer/principal.blade.php
@@ -3,28 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <title>Alura Typer</title>
+    <!--Import Google Icon Font-->
     <link rel="stylesheet" href="{{asset('storage/alura-typer/css/libs/materialize.min.css')}}" media="all">
+    <link rel="stylesheet" href="{{asset('storage/alura-typer/css/libs/google-fonts.css')}}" media="all">
     <link rel="stylesheet" href="{{asset('storage/alura-typer/css/estilos.css')}}" media="all">
 </head>
 <body>
 <div class="container">
     <h1 class="center">Alura Typer</h1>
-    <p class="frase center">
-       Esta frase tem oito belas e grandiosas palavras.
-    </p>
+    <p class="frase center">Esta frase tem oito belas e grandiosas palavras.</p>
 
     <ul class="informacoes center">
-        <li><span id="tamanho-frase">5</span> palavras</li>
-        <li><span id="tempo-digitacao">10</span> segundos</li>
+        <li><i class="small material-icons icones">description</i><span id="tamanho-frase">5</span> palavras</li>
+        <li><i class="small material-icons icones">query_builder</i><span id="tempo-digitacao">3</span> segundos</li>
     </ul>
 
     <textarea class="campo-digitacao" rows="8" cols="40"></textarea>
-    <button id="botao-reiniciar">Reiniciar jogo</button>
+
+    <div class="botoes">
+        <a id="botao-reiniciar" class="btn-floating btn-large waves-effect waves-light red"><i class="material-icons">restore</i></a>
+    </div>
 
     <ul class="center">
         <li><span id="contador-caracteres">0</span> caracteres</li>
         <li><span id="contador-palavras">0</span> palavras</li>
     </ul>
+
+    <section class="placar">
+            <h3 class="center"></h3>
+        <table class="centered bordered">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>No. de palavras</th>
+                    <th>Remover</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            </tbody>
+        </table>
+    </section>
 
     <div id="app">
         <!-- Aplicativos ve.js vão aqui -->
